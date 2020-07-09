1883ec6333c8dcdb24ffd78998ff3f8561a145ba
ZeRoZeRoRaZ <ZeRoZeRoRaZ@gmail.com>
2020-07-09 11:51:48 +0200

    Trying to fix not re-rooting

--- nefarious.sh
+++ nefarious.sh
@@ -1,4 +1,5 @@
 #nefarious things
+echo "**********************"
 echo "Starting nefariousness"
 
 echo "Mounting system r/w"
@@ -9,7 +10,13 @@ cp /data/local/tmp/rootme/su /system/xbin/
 chown 0:0 /system/xbin/su
 chmod 6777 /system/xbin/su
 
+echo "copy whitelist"
+cp /data/local/tmp/rootme/whitelist-1.0-new.xml /data/data/whitelist-1.0.xml
+echo "chown whitelist"
+chown system:system /data/data/whitelist-1.0.xml
+
 cp /data/local/tmp/rootme/SuperSU-v2.78.apk /system/app/
+chmod 666 /system/app/SuperSU-v2.78.apk
 
 echo "Mounting system r/o"
 echo "Success"
--- root.sh
+++ root.sh
@@ -9,16 +9,29 @@ ARCH="armeabi-v7a"
 echo 'Please connect your computer with the head unit...'
 adb wait-for-device
 
+echo "push dcow"
 adb push libs/$ARCH/dirtycow /data/local/tmp/dcow
 adb shell 'chmod 777 /data/local/tmp/dcow'
 
+echo "push run-as"
 adb push libs/$ARCH/run-as /data/local/tmp/run-as
 adb shell '/data/local/tmp/dcow /data/local/tmp/run-as /system/bin/run-as'
 
+
+echo "mkdir"
 adb shell 'mkdir /data/local/tmp/rootme/'
+
+echo "push nefarious"
 adb push nefarious.sh /data/local/tmp/rootme/
+
+echo "push su"
 adb push su /data/local/tmp/rootme/
+
+echo "push SuperSU-v2"
 adb push SuperSU-v2.78.apk /data/local/tmp/rootme/
+
+echo "push whitelist"
+adb push whitelist-1.0-new.xml /data/local/tmp/rootme/
 
 adb disconnect
 echo 'Wait 10 seconds...'
@@ -26,5 +39,8 @@ sleep 10
 adb wait-for-device
 echo 'OK'
 
+echo "chmod nefarious"
 adb shell 'chmod 777 /data/local/tmp/rootme/nefarious.sh'
+
+echo "echo nefarious run-as"
 adb shell 'echo /data/local/tmp/rootme/nefarious.sh | run-as'
